1f8dc87e2ea98651b6456af95910fe529ab39abc
jurandorcod <jurandiraparecido19651965@gmail.com>
2024-08-07 16:47:31 -0300

    salvar no banco

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,10 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
+        
+        //dd($request->all());
+        $contato = new SiteContato;
+        /*$contato->nome = $request->input('nome');
+        $contato->email = $request->input('email');
+        $contato->telefone = $request->input('telefone');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+        */
+        //$contato->fill($request->all());
+        //$contato->save();
+        //print_r($contato->getAttributes());
+
         return view('site.contato', ['titulo' => 'contato (teste)']);
+    }
+
+    public function salvar(Request $request){
+        
+        $contato = new SiteContato;
+        $contato->fill($request->all());
+        $contato->save();
+        return redirect()->route('site.contato');
     }
 }
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -10,5 +10,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    //
+    protected $fillable = ['nome', 'email', 'telefone', 'motivo_contato', 'mensagem'];
 }
